1181a5a8e65c8eaec1b18fd5d4dca059f5032009
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-07-23 11:45:58 +0700

    edit Main and service

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    public double calculate(double month) {
-        double m1 = 9.99 / 12 / 100; // Константа
-        double payment = (m1 * Math.pow(1 + m1, month)) / (Math.pow(1 + m1, month) - 1); // Расчет процентов
+    public double calculate(double percent, double month, double mortgageAmount) {
+        double m1 = percent / 12 / 100; // Константа
+        double payment = (m1 * Math.pow(1 + m1, month)) / (Math.pow(1 + m1, month) - 1) * mortgageAmount; // Расчет процентов
         return (payment);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int mortgageAmount = 1_000_000;
-        int month = 36;
-        int paymentForMonth = (int) (service.calculate(month) * mortgageAmount); // Расчет платежа
-        System.out.println("Месячный платеж: " + paymentForMonth);
+
+        int paymentFor12Month = (int) service.calculate(9.99, 12, 1_000_000); // Расчет платежа
+        System.out.println("Месячный платеж: " + paymentFor12Month);
+
+        int paymentFor24Month = (int) service.calculate(9.99, 24, 1_000_000); // Расчет платежа
+        System.out.println("Месячный платеж: " + paymentFor24Month);
+
+        int paymentFor36Month = (int) service.calculate(9.99, 36, 1_000_000); // Расчет платежа
+        System.out.println("Месячный платеж: " + paymentFor36Month);
     }
 }
